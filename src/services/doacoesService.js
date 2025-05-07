e44eeb86fe4b72d89efcb45262d0e4f2543bdedc
Kenniaf <projetointegradorufms@gmail.com>
2025-05-07 00:45:02 -0300

    atualização

--- src/services/doacoesService.js
+++ src/services/doacoesService.js
@@ -2,15 +2,15 @@ import api from './api'
 
 export default {
   criarDoacao(doacao) {
-    return api.post('/doacoes', doacao)
+    return api.post('/doacoes/', doacao) // Removido /api do início
   },
   listarDoacoes() {
-    return api.get('/doacoes')
+    return api.get('/doacoes') // Removido /api do início
   },
   atualizarDoacao(id, dados) {
-    return api.put(`/doacoes/${id}`, dados)
+    return api.put(`/doacoes/${id}`, dados) // Removido /api do início
   },
   excluirDoacao(id) {
-    return api.delete(`/doacoes/${id}`)
+    return api.delete(`/doacoes/${id}`) // Removido /api do início
   }
 }
